091689532217af1356e3f7920c1d7fe25e67487c
Piyush Yadav <gitforpiyush@gmail.com>
2026-02-07 01:23:13 +0530

    added firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -52,5 +53,8 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
     implementation("com.google.firebase:firebase-analytics")
+    //firestore
+    implementation("com.google.firebase:firebase-firestore")
+
 
 }
